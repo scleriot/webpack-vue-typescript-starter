e458665e166ab3c8ff0da117d1e9192dd8569815
Simon Clériot <cleriotsimon@gmail.com>
2017-08-17 17:00:34 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Starter App'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
